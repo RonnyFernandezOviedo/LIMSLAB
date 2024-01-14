f2903f6af8250922325206d0758b1e0bd31e5648
RonnyFernandezOviedo <feroviro86@gmail.com>
2024-01-13 20:22:46 -0600

    first commit

--- webapp/conf.ts
+++ webapp/conf.ts
@@ -7,8 +7,4 @@ export const NODE_ENV='development';
 <<<<<<< HEAD
 export const GQL_BASE_URL = 'http://192.168.100.191:8000/felicity-gql'; //import.meta.env.VITE_BASE_GRAPHQL_URL ;http://172.20.2.219:5173/
 export const WS_BASE_URL = 'ws://127.0.0.1:8000/felicity-gql'; //import.meta.env.VITE_BASE_WS_URL;
-=======
-export const GQL_BASE_URL = 'http://192.168.100.191:8000/felicity-gql'; //import.meta.env.VITE_BASE_GRAPHQL_URL ;
-export const WS_BASE_URL = 'ws://localhost:8000/felicity-gql'; //import.meta.env.VITE_BASE_WS_URL;
->>>>>>> 89903479d57386208ecabf4d77b34c3243aae2d0
 export const REST_BASE_URL = 'http://127.0.0.1:8000' //import.meta.env.VITE_BASE_URL ;
