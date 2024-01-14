43daa7391055b24651f783be70ffe1453d5ded86
RonnyFernandezOviedo <feroviro86@gmail.com>
2024-01-14 11:59:14 -0600

    first commit

--- webapp/conf.ts
+++ webapp/conf.ts
@@ -5,5 +5,5 @@ export const ENCRYPT_AUTH_KEY = 'felicity';
 export const USER_GROUP_OVERRIDE = ''; // 'ADMINISTRATOR'
 export const NODE_ENV='development';
 export const GQL_BASE_URL = 'http://192.168.100.191:8000/felicity-gql'; //import.meta.env.VITE_BASE_GRAPHQL_URL ;http://172.20.2.219:5173/
-export const WS_BASE_URL = 'ws://127.0.0.1:8000/felicity-gql'; //import.meta.env.VITE_BASE_WS_URL;
+export const WS_BASE_URL = 'ws://192.168.100.191:8000/felicity-gql'; //import.meta.env.VITE_BASE_WS_URL;
 export const REST_BASE_URL = 'http://127.0.0.1:8000' //import.meta.env.VITE_BASE_URL ;
